71cbd55e67acdc7aadf790127a3f724113de75cf
Colin Tück <colin.tueck@eqar.eu>
2022-02-15 20:58:31 +0100

    stats: nicer URLs with optional .<format> suffix

--- api/stats/urls.py
+++ api/stats/urls.py
@@ -2,12 +2,16 @@ from django.urls import path, include
 
 from stats.views import *
 
-urlpatterns = [
-    path(r'esg/simple/', ComplianceStats.as_view()),
-    path(r'esg/extended/', ComplianceExtendedStats.as_view()),
-    path(r'applications/open/', OpenApplications.as_view()),
-    path(r'applications/withdrawn/', WithdrawnApplications.as_view()),
-    path(r'applications/by-year/', ApplicationsTimeline.as_view()),
-]
+class UrlList(list):
 
+    def add_url(self, url, view):
+        self.append(path(f'{url}/', view.as_view()))
+        self.append(path(f'{url}.<slug:format>/', view.as_view()))
+
+urlpatterns = UrlList()
+urlpatterns.add_url(r'esg/simple', ComplianceStats)
+urlpatterns.add_url(r'esg/extended', ComplianceExtendedStats)
+urlpatterns.add_url(r'applications/open', OpenApplications)
+urlpatterns.add_url(r'applications/withdrawn', WithdrawnApplications)
+urlpatterns.add_url(r'applications/by-year', ApplicationsTimeline)
 
