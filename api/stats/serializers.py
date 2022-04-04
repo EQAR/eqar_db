4a0ba6824c15e966371256aadcbc714193a06ec5
Colin Tück <colin.tueck@eqar.eu>
2022-04-05 01:20:15 +0200

    [stats] add agency country to application list

--- api/stats/serializers.py
+++ api/stats/serializers.py
@@ -1,4 +1,4 @@
-from contacts.models import Contact, Organisation
+from contacts.models import Contact, Organisation, Country
 from agencies.models import Applications, RegisteredAgency
 from uni_db.serializers import ListSerializer
 
@@ -7,6 +7,12 @@ class _OrganisationSerializer(ListSerializer):
     class Meta:
         model = Organisation
         fields = [ "id", "longname", "acronym" ]
+
+
+class _CountrySerializer(ListSerializer):
+    class Meta:
+        model = Country
+        fields = [ "iso3", "iso2", "name", "ehea", "eu", "eter" ]
 
 
 class _ContactSerializer(ListSerializer):
@@ -18,10 +24,11 @@ class _ContactSerializer(ListSerializer):
 
 class _AgencySerializer(ListSerializer):
     organisation = _OrganisationSerializer()
+    baseCountry = _CountrySerializer()
 
     class Meta:
         model = RegisteredAgency
-        fields = [ "id", "organisation", "shortname", "registered", "deqarId" ]
+        fields = [ "id", "organisation", "shortname", "registered", "deqarId", "baseCountry" ]
 
 
 class ApplicationsListSerializer(ListSerializer):
